018d23f3df0620c2895e84607caf072c0b8d1f31
Nigh7Sh4de <plotnikd@gmail.com>
2016-03-09 15:13:30 -0500

    Add AngularJS and improve navbar

---
+++ public/js/app.js
@@ -0,0 +1,34 @@
+var roundawayApp = roundawayApp || angular.module('roundawayApp', ['ngRoute']);
+
+roundawayApp.config(function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+
+    $routeProvider
+        .when('/', {
+            templateUrl: 'home.html',
+            controller: 'homeCtrl'
+        })
+        .when('/login', {
+            templateUrl: 'login.html',
+            controller: 'loginCtrl'
+        });
+})
+
+var roundawayCtrl = function($scope) {
+    console.log('App started.');
+}
+
+var homeCtrl = function($scope) {
+    console.log('Home page');
+};
+
+var loginCtrl = function($scope) {
+    console.log('Login page');
+};
+
+roundawayApp.controller('roundawayCtrl', roundawayCtrl);
+roundawayApp.controller('homeCtrl', homeCtrl);
+roundawayApp.controller('loginCtrl', loginCtrl);
--- server.js
+++ server.js
@@ -3,6 +3,25 @@ var app = express();
 
 app.use(express.static('public'));
 
+app.get('/404', function(req, res) {
+    res.send('404');
+});
+
+app.get('/node_modules/angular/angular.js', function(req, res) {
+    res.sendFile(__dirname + '/node_modules/angular/angular.js');
+});
+
+app.get('/node_modules/angular-route/angular-route.js', function(req, res) {
+    res.sendFile(__dirname + '/node_modules/angular-route/angular-route.js');
+});
+
+app.get('/', sendIndex);
+app.get('/login', sendIndex);
+
+function sendIndex(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+}
+
 app.listen(8080, function() {
     console.log('Example app listening on port 8080!');
 });
