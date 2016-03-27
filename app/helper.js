d7f0b04acc446298f67bab04bb2435b33da7c3d4
nigh7sh4de <plotnikd@gmail.com>
2016-03-27 13:56:31 -0400

    Make helper.js more mockable

--- app/helper.js
+++ app/helper.js
@@ -1,23 +1,29 @@
-module.exports = function(app) {
-    app.allowGet = function(file) {
-        app.get(file, function(req, res) {
+var helper = function(obj) {
+    Object.assign(obj, exts);
+}
+
+var exts = {
+    allowGet: function(file) {
+        this.get(file, function(req, res) {
             return res.sendFile(file, { root: __dirname + '/..' });
         });
-    }
+    },
 
-    app.sendIndex = function(req, res) {
+    sendIndex: function(req, res) {
         return res.sendFile('/public/index.html', { root: __dirname + '/..' });
-    }
+    },
 
-    app.checkAuth = function(req, res, next) {
+    checkAuth: function(req, res, next) {
         if (req.isAuthenticated())
             return next();
         res.redirect('/login');
-    }
+    },
 
-    app.checkAdmin = function(req, res, next) {
+    checkAdmin: function(req, res, next) {
         if (req.user.admin)
             return next();
         res.redirect('/home');
     }
 }
+
+module.exports = Object.assign(helper, exts);
