02483057445e21e6766feb7cc9962d263f2d69d5
nigh7sh4de <plotnikd@gmail.com>
2016-04-02 18:01:35 -0400

    Booking schema and tests

--- app/models/Booking.js
+++ app/models/Booking.js
@@ -1,7 +1,86 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var bookingSchema = new Schema();
+var bookingSchema = new Schema({
+    archived: {
+        type: Boolean,
+        default: false
+    },
+    spot: {
+        type: String,
+    },
+    start: {
+        type: Date
+    },
+    duration: {
+        type: Number
+    }
+});
+
+bookingSchema.methods.setEndTime = function(time, cb) {
+    if (!(time instanceof Date))
+        return cb(new Error('Cannot set end time. Provided date is invalid.'));
+    this.duration = time - this.start;
+    this.save(cb);
+}
+
+bookingSchema.methods.getEndTime = function() {
+    var start = this.getStartTime();
+    if (start instanceof Error)
+        return start;
+    var duration = this.getDuration();
+    if (duration instanceof Error)
+        return duration;
+    return new Date(start.valueOf() + duration);
+    
+}
+
+bookingSchema.methods.setDuration = function(dur, cb) {
+    if (typeof dur !== 'number' || dur <= 0)
+        return cb(new Error('Cannot set duration. Provided duration is invalid.'));
+    this.duration = dur;
+    this.save(cb);
+}
+
+bookingSchema.methods.getDuration = function() {
+    if (this.duration != null)
+        return this.duration;
+    else
+        return new Error('This booking does not have a duration set.');
+}
+
+bookingSchema.methods.setStartTime = function(time, cb) {
+    if (!(time instanceof Date))
+        return cb(new Error('Cannot set start time. Provided date is invalid.'));
+    this.start = time;
+    this.save(cb);
+}
+
+bookingSchema.methods.getStartTime = function() {
+    if (this.start != null)
+        return this.start;
+    else
+        return new Error('This booking does not have a start time set.');
+}
+
+bookingSchema.methods.setSpotId = function(id, cb) {
+    if (typeof id !== 'string' || id == '')
+        return cb(new Error('Cannot set spot for this booking. Provided ID is invalid.'));
+    this.spot = id;
+    this.save(cb);
+}
+
+bookingSchema.methods.getSpotId = function() {
+    if (this.spot != null)
+        return this.spot;
+    else
+        return new Error('This booking does not have a spot associated with it.');
+}
+
+bookingSchema.methods.archive = function(cb) {
+    this.archived = true;
+    this.save(cb);
+}
 
 var Booking = mongoose.model('Booking', bookingSchema);
 
---
+++ app/tests/booking.test.js
@@ -0,0 +1,216 @@
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var Booking = require('./../models/Booking');
+
+describe('Booking schema', function() {
+    before(function() {
+        sinon.stub(Booking.prototype, 'save', function(cb) { cb() });
+    })
+    
+    after(function() {
+        Booking.prototype.save.restore();
+    })
+    
+    describe('archive', function() {
+        it('should set archived to true', function(done) {
+            var b = new Booking();
+            expect(b.archived).to.be.false;
+            b.archive(function(err) {
+                expect(err).to.not.be.ok;
+                expect(b.archived).to.be.true;
+                done();
+            })
+        })
+    })
+    
+    describe('getSpotId', function() {
+        it('should return spot id', function() {
+            var b = new Booking();
+            var spotId = '1z2x3c4v';
+            b.spot = spotId;
+            expect(b.getSpotId()).to.equal(spotId);
+        })
+        
+        it('should error if no spot id', function() {
+            var b = new Booking();
+            var result = b.getSpotId();
+            expect(result).to.be.an.instanceof(Error);
+        })
+    })
+    
+    describe('setSpotId', function() {
+        it('should set spot id', function(done) {
+            var b = new Booking();
+            var spotId = '1z2x3c4v';
+            expect(b.spot).to.not.be.ok;
+            b.setSpotId(spotId, function(err) {
+                expect(err).to.not.be.ok;
+                expect(b.spot).to.equal(spotId);
+                done();
+            });
+        })
+        
+        it('should error if invalid spot id', function(done) {
+            var b = new Booking();
+            [
+                null,
+                undefined,
+                123,
+                true,
+                ''
+            ].forEach(function (input, i, arr) {
+                expect(b.setSpotId(input, function(err) {
+                    expect(err).to.be.ok;
+                    expect(b.spot).to.not.be.ok;
+                    if (i + 1 >= arr.length)
+                        done();
+                }));
+            })
+        })
+    })
+    
+    describe('getStartTime', function() {
+        it('should return start time', function() {
+            var b = new Booking();
+            var start = new Date();
+            b.start = start;
+            expect(b.getStartTime()).to.equal(start);
+        })
+        
+        it('should error if no start time set', function() {
+            var b = new Booking();
+            var result = b.getStartTime();
+            expect(result).to.be.an.instanceof(Error);
+        })
+    })
+    
+    describe('setStartTime', function() {
+        it('should set start time', function(done) {
+            var b = new Booking();
+            var start = new Date();
+            expect(b.start).to.not.be.ok;
+            b.setStartTime(start, function(err) {
+                expect(err).to.not.be.ok;
+                expect(b.start).to.equal(start);
+                done();
+            });
+        })
+        
+        it('should error if invalid start time', function(done) {
+            var b = new Booking();
+            [
+                null,
+                undefined,
+                123,
+                true,
+                ''
+            ].forEach(function (input, i, arr) {
+                expect(b.setStartTime(input, function(err) {
+                    expect(err).to.be.ok;
+                    expect(b.start).to.not.be.ok;
+                    if (i + 1 >= arr.length)
+                        done();
+                }));
+            })
+        })
+    })
+    
+    describe('getDuration', function() {
+        it('should return duration', function() {
+            var b = new Booking();
+            var duration = new Date() - new Date('01/01/2000');
+            b.duration = duration;
+            expect(b.getDuration()).to.equal(duration);
+        })
+        
+        it('should error if no start time set', function() {
+            var b = new Booking();
+            var result = b.getDuration();
+            expect(result).to.be.an.instanceof(Error);
+        })
+    })
+    
+    describe('setDuration', function() {
+        it('should set start', function(done) {
+            var b = new Booking();
+            var duration = new Date() - new Date('01/01/2000');
+            expect(b.duration).to.not.be.ok;
+            b.setDuration(duration, function(err) {
+                expect(err).to.not.be.ok;
+                expect(b.duration).to.equal(duration);
+                done();
+            });
+        })
+        
+        it('should error if invalid duration', function(done) {
+            var b = new Booking();
+            [
+                null,
+                undefined,
+                -1,
+                true,
+                ''
+            ].forEach(function (input, i, arr) {
+                expect(b.setDuration(input, function(err) {
+                    expect(err).to.be.ok;
+                    expect(b.duration).to.not.be.ok;
+                    if (i + 1 >= arr.length)
+                        done();
+                }));
+            })
+        })
+    })
+    
+    describe('getEndTime', function() {
+        it('should return end time', function() {
+            var b = new Booking();
+            var start = new Date('01/01/2000');
+            var end = new Date();
+            var duration = end - start; 
+            b.start = start;
+            b.duration = duration;
+            expect(b.getEndTime()).to.deep.equal(end);
+        })
+        
+        it('should error if no end time set', function() {
+            var b = new Booking();
+            var result = b.getEndTime();
+            expect(result).to.be.an.instanceof(Error);
+        })
+    })
+    
+    describe('setEndTime', function() {
+        it('should set end time', function(done) {
+            var b = new Booking();
+            var start = new Date('01/01/2000');
+            var end = new Date();
+            var duration = end - start;
+            b.start = start;
+            b.duration = 10;
+            b.setEndTime(end, function(err) {
+                expect(err).to.not.be.ok;
+                expect(b.duration).to.equal(duration);
+                done();
+            });
+        })
+        
+        it('should error if invalid end time', function(done) {
+            var b = new Booking();
+            var dur = b.duration;
+            [
+                null,
+                undefined,
+                123,
+                true,
+                ''
+            ].forEach(function (input, i, arr) {
+                expect(b.setEndTime(input, function(err) {
+                    expect(err).to.be.ok;
+                    expect(b.duration).to.equal(dur);
+                    if (i + 1 >= arr.length)
+                        done();
+                }));
+            })
+        })
+    })
+})
