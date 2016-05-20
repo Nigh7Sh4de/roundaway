a0d60deec38686780aa53c58a1ee6156b58c8cf3
nigh7sh4de <plotnikd@gmail.com>
2016-05-20 11:22:57 -0400

    Implement Spot controller get set for collection, location, and bookings

--- app/controllers/spotController.js
+++ app/controllers/spotController.js
@@ -1,3 +1,5 @@
+var Spot = require('./../models/Spot');
+
 var controller = function(app) {
     this.app = app;
     app.get('/api/spots', app.checkAuth, app.checkAdmin, this.GetAllSpots.bind(this));
@@ -6,7 +8,7 @@ var controller = function(app) {
     app.get('/api/spots/:id/location', app.checkAuth, app.checkAdmin, this.GetLocationForSpot.bind(this));
     app.post('/api/spots/:id/location', app.checkAuth, app.checkAdmin, app.bodyParser.json(), this.SetLocationForSpot.bind(this));
     app.get('/api/spots/:id/bookings', app.checkAuth, app.checkAdmin, this.GetAllBookingsForSpot.bind(this));
-    app.put('/api/spots/:id/bookings', app.checkAuth, app.checkAdmin, app.bodyParser.json(), this.AdBookingsToSpot.bind(this));
+    app.put('/api/spots/:id/bookings', app.checkAuth, app.checkAdmin, app.bodyParser.json(), this.AddBookingsToSpot.bind(this));
     app.delete('/api/spots/:id/bookings', app.checkAuth, app.checkAdmin, app.bodyParser.json(), this.RemoveBookingsFromSpot.bind(this));
     app.get('/api/spots/:id/available', app.checkAuth, app.checkAdmin, this.GetAllAvailabilityForSpot.bind(this));
     app.put('/api/spots/:id/available', app.checkAuth, app.checkAdmin, app.bodyParser.json(), this.AdAvailabilityToSpot.bind(this));
@@ -22,25 +24,194 @@ controller.prototype = {
         });
     },
     CreateSpot: function(req, res) {
-        
+        var newSpot = new Spot(req.body.spot).toJSON();
+        delete newSpot._id;
+        if (req.body.count != null) {
+            if (typeof req.body.count !== 'number' || req.body.count <= 0)
+                return res.status(500).send(new Error('Could not create spot. Specified count was invalid.'));
+            var arr = [];
+            for (var i=0;i<req.body.count;i++)
+                arr.push(newSpot);
+            this.app.db.spots.collection.insert(arr, function(err, result) {
+                if (err != null)
+                    return res.send({status: 'ERROR', error: err});
+                res.send({status: 'SUCCESS', result: result});
+            })
+        }
+        else {
+            this.app.db.spots.create(newSpot, function(err, result) {
+                if (err != null)
+                    return res.send({status: 'ERROR', error: err});
+                res.send({status: 'SUCCESS', result: result});
+            })    
+        }
     },
     GetSpot: function(req, res) {
-        
+        this.app.db.spots.findById(req.params.id, function(err, doc) {
+            if (err != null)
+                return res.status(500).send(err.message);
+            else if (doc == null)
+                return res.status(500).send('Spot not found.');
+            else
+                return res.send(doc);
+        })
     },
     GetLocationForSpot: function(req, res) {
-        
+        var app = this.app;
+        app.db.spots.findById(req.params.id, function(err, doc) {
+            if (err != null)
+                return res.status(500).send(err.message);
+            else if (doc == null)
+                return res.status(500).send('Spot not found.');
+            else {
+                var loc = {
+                    address: doc.getAddress(),
+                    coordinates: doc.getLocation()
+                }
+                return res.send(loc);
+                
+            }
+        })
     },
     SetLocationForSpot: function(req, res) {
-        
+        var coords = req.body.coordinates;
+        var app = this.app;
+        app.db.spots.findById(req.params.id, function(err, spot) {
+            if (err != null) {
+                return res.status(500).send(err.message);
+            }
+            else {
+                if (coords instanceof Array)
+                    coords = {lat:coords[0], lon:coords[1]};
+                if (coords.lon == null)
+                    coords.lon = coords.long;
+                if (coords.long !== undefined)
+                    delete coords.long;
+                app.geocoder.reverse(coords, function(err, loc) {
+                    loc = loc[0];
+                    var c = 0,
+                        total = 2;
+                    var next = function(err) {
+                        if (err != null)
+                            return res.status(500).send(err.message);
+                        if(++c >= total)
+                            done();
+                    }
+                    var done = function() {
+                        res.sendStatus(200);
+                    }
+                    spot.setAddress(loc.formattedAddress, next);
+                    spot.setLocation(coords, next);
+                })    
+            }
+        });
     },
     GetAllBookingsForSpot: function(req, res) {
-        
+        var app = this.app;
+        app.db.spots.findById(req.params.id, function(err, doc) {
+            if (err != null)
+                return res.status(500).send(err.message);
+            else if (doc == null)
+                return res.status(500).send('Spot not found.');
+            else {
+                var result = [];
+                var bookings = doc.getBookings();
+                var next = function(obj) {
+                    result.push(obj);
+                    if (result.length >= bookings.length)
+                        return res.send(result);
+                }
+                if (bookings.length > 0)
+                    bookings.forEach(function(booking) {
+                        app.db.bookings.findById(booking, function(err, doc) {
+                            if (err != null)
+                                next('Could not find booking ' + booking + ': ' + err.message);
+                            else if (doc == null)
+                                next('Could not find booking ' + booking);
+                            else
+                                next(doc);
+                        })
+                    });
+                else
+                    return res.send(bookings);
+            }
+        })
     },
-    AdBookingsToSpot: function(req, res) {
-        
+    AddBookingsToSpot: function(req, res) {
+        var app = this.app;
+        if (!req.body.bookings)
+            return res.status(500).send('Cannot add bookings. Must supply bookings parameter.');
+        if (!(req.body.bookings instanceof Array))
+            req.body.bookings = [req.body.bookings];
+        var firstType = typeof req.body.bookings[0];
+        req.body.bookings.forEach(function(booking) {
+            if (typeof booking !== firstType || (typeof booking !== 'string' && typeof booking !== 'object'))
+                return res.status(500).send('Cannot add bookings. All bookings must be either id\'s or objects.');
+            else if (typeof booking === 'object' && (!booking.id || !booking.start || !booking.end))
+                return res.status(500).send('Cannot add bookings. Booking objects must have properties: id, start, end');
+        });
+        app.db.spots.findById(req.params.id, function(err, doc) {
+            if (err != null)
+                return res.status(500).send(err.message);
+            else if (doc == null)
+                return res.status(500).send('Spot not found.');
+            else {
+                var addBookings = function(bookings) {
+                    doc.addBookings(bookings, function(err) {
+                        if (err != null)
+                            return res.status(500).send(err);
+                        else
+                            return res.sendStatus(200);
+                    })
+                }
+                if (typeof req.body.bookings[0] === 'string')
+                    app.db.bookings.find({_id: {$in: req.body.bookings}}, function(err, bookings) {
+                        if (err != null)
+                            return res.status(500).send(err);
+                        addBookings(bookings);
+                    })
+                else
+                    addBookings(req.body.bookings);
+            }
+        });
     },
     RemoveBookingsFromSpot: function(req, res) {
-        
+        var app = this.app;
+        if (!req.body.bookings)
+            return res.status(500).send('Cannot remove bookings. Must supply bookings parameter.');
+        if (!(req.body.bookings instanceof Array))
+            req.body.bookings = [req.body.bookings];
+        var firstType = typeof req.body.bookings[0];
+        req.body.bookings.forEach(function(booking) {
+            if (typeof booking !== firstType || (typeof booking !== 'string' && typeof booking !== 'object'))
+                return res.status(500).send('Cannot remove bookings. All bookings must be either id\'s or objects.');
+            else if (typeof booking === 'object' && (!booking.id || !booking.start || !booking.end))
+                return res.status(500).send('Cannot remove bookings. Booking objects must have properties: id, start, end');
+        });
+        app.db.spots.findById(req.params.id, function(err, doc) {
+            if (err != null)
+                return res.status(500).send(err.message);
+            else if (doc == null)
+                return res.status(500).send('Spot not found.');
+            else {
+                var removeBookings = function(bookings) {
+                    doc.removeBookings(bookings, function(err) {
+                        if (err != null)
+                            return res.status(500).send(err);
+                        else
+                            return res.sendStatus(200);
+                    })
+                }
+                if (typeof req.body.bookings[0] === 'string')
+                    app.db.bookings.find({_id: {$in: req.body.bookings}}, function(err, bookings) {
+                        if (err != null)
+                            return res.status(500).send(err);
+                        removeBookings(bookings);
+                    })
+                else
+                    removeBookings(req.body.bookings);
+            }
+        });
     },
     GetAllAvailabilityForSpot: function(req, res) {
         
