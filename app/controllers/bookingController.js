ef29c4754d606ccd90a531883d1cc99b8ef68b65
nigh7sh4de <plotnikd@gmail.com>
2016-06-07 10:14:02 -0400

    bookingControl should send strings not Error objects

--- app/controllers/bookingController.js
+++ app/controllers/bookingController.js
@@ -43,7 +43,7 @@ controller.prototype = {
         delete newBooking._id;
         if (req.body.count != null) {
             if (typeof req.body.count !== 'number' || req.body.count <= 0)
-                return res.status(500).send(new Error('Could not create booking. Specified count was invalid.'));
+                return res.status(500).send('Could not create booking. Specified count was invalid.');
             var arr = [];
             for (var i=0;i<req.body.count;i++)
                 arr.push(newBooking);
