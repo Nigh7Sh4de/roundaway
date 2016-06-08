ac362583036428c804862615950ff0da495b3c22
nigh7sh4de <plotnikd@gmail.com>
2016-06-08 16:51:21 -0400

    oops

--- app/tests/routeTestBase.js
+++ app/tests/routeTestBase.js
@@ -47,7 +47,7 @@ function HappyPathRouteTest(ctrl, verb, route, ignoreAdmin, ignoreAuth, method, 
     st.expect(200).end(function (err, res) {
         expect(err).to.not.be.ok;
         if (testOutput != null) {
-            var expectedOutput = JSON.stringify(testOutput).slice(1, expectedOutput.length - 1);
+            var expectedOutput = JSON.stringify(testOutput).slice(1, testOutput.length - 1);
             expect(res.text).to.contain(expectedOutput);
         }
         if (assertions != null && typeof assertions === 'function')
