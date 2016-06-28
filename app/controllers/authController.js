9b358be09a37be08b16e3ec2fe7eae4785cb0c25
nigh7sh4de <plotnikd@gmail.com>
2016-06-28 16:11:58 -0400

    Refactor auth controller to be more modular

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -2,18 +2,31 @@ var authController = function(app) {
         this.app = app;
         app.get('/logout', this.Logout);
 
-        app.get('/login/google', this.Login('google'));
-        app.get('/login/google/return', this.LoginReturn('google'));
-        app.get('/auth/google', this.LoginReturn('google-token'));
-        app.get('/connect/google', this.Connect('google'));
-        app.get('/connect/google/return', this.ConnectReturn('google'));
+        app.get('/login/:strat', this.Login.bind(this));
+        app.get('/login/:strat/return', this.LoginReturn.bind(this));
+        app.get('/auth/:strat', this.LoggedIn.bind(this));
+        app.get('/connect/:strat', this.Connect.bind(this));
+        app.get('/connect/:strat/return', this.ConnectReturn.bind(this));
 
-        app.get('/login/facebook', this.Login('facebook'));
-        app.get('/login/facebook/return', this.LoginReturn('facebook'));
-        app.get('/auth/facebook', this.LoginReturn('facebook-token'));
-        app.get('/connect/facebook', this.Connect('facebook'));
-        app.get('/connect/facebook/return', this.ConnectReturn('facebook'));
     }
+
+var GenerateCallback = function(redirect, req, res) {
+    return redirect ? {
+        failureRedirect: '/login',
+        successRedirect: '/home'
+    } : 
+    function(err, user, info) {
+        if (err)
+            return res.sendBad(err);
+        if (!user)
+            return res.sendBad('User could not be authenticated');
+        req.login(user, function(err) {
+            if (err)
+                return res.sendBad(err);
+            res.sendGood('User authenticated successfuly', user);
+        })
+    }
+}
 
 authController.prototype = {
     app: null,
@@ -27,24 +40,32 @@ authController.prototype = {
         req.logout();
         res.redirect('/');
     },
-    Login: function(strat) {
-        return this.app.passport.authenticate(strat, this.strategies[strat].authProps);    
+    Login: function(req, res, next) {
+        var strat = req.params.strat;
+        this.app.passport.authenticate(strat, this.strategies[strat].authProps)(req, res, next);
     },
-    LoginReturn: function(strat) {
-        return this.app.passport.authenticate(strat, {
+    LoginReturn: function(req, res, next) {
+        this.app.passport.authenticate(
+            req.params.strat,  
+            GenerateCallback(req.query.noredirect === undefined, req, res)
+        )(req, res, next);
+    },
+    LoggedIn: function(req, res, next) {
+        this.app.passport.authenticate(req.params.strat, {
             failureRedirect: '/login',
             successRedirect: '/home'
-        })
+        })(req, res, next);
     },
-    Connect: function(strat) {
-        return this.app.passport.authorize(strat, this.strategies[strat].authProps);
+    Connect: function(req, res, next) {
+        var strat = req.params.strat;
+        this.app.passport.authorize(strat, this.strategies[strat].authProps)(req, res, next);
     },
-    ConnectReturn: function(strat) {
-        return this.app.passport.authorize(strat, {
+    ConnectReturn: function(req, res, next) {
+        this.app.passport.authorize(req.params.strat, {
             failureRedirect: '/login',
             successRedirect: '/profile'
-        })
-    },
+        })(req, res, next);
+    }
     // init: function(app) {
     //     this.app = app;
     //     app.get('/logout', this.Logout);
