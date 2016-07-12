6632fdad6675b5293d31c3bbb2c3236fc4fc810d
nigh7sh4de <plotnikd@gmail.com>
2016-07-12 10:23:37 -0400

    Improve mockPromise object

--- app/tests/mockPromise.js
+++ app/tests/mockPromise.js
@@ -1,17 +1,16 @@
 module.exports = function(resolveWith, errorWith) {
     return function() {
-        return {
-            populate: function() {
-                return this;
-            },
-            exec: function() {
-                return new Promise(function(resolve, reject) {
-                    if (errorWith)
-                        reject(errorWith);
-                    else
-                        resolve(resolveWith);
-                });;
-            }
+        var promise = new Promise(function(resolve, reject) {
+            if (errorWith)
+                reject(errorWith);
+            else
+                resolve(resolveWith);
+        });;
+        var returnThis = function() {
+            return this;
         }
+        promise.populate = returnThis;
+        promise.exec = returnThis;
+        return promise;
     }
 }
