c011bdccc08f8ab0758f51f5920171219e14fd25
nigh7sh4de <plotnikd@gmail.com>
2016-07-14 12:31:23 -0400

    Make mockPromise into a mock

--- app/tests/mockPromise.js
+++ app/tests/mockPromise.js
@@ -1,4 +1,6 @@
-module.exports = function(resolveWith, errorWith) {
+var sinon = require('sinon');
+
+module.exports = function(resolveWith, errorWith, assert, assert_on) {
     return function() {
         var promise = new Promise(function(resolve, reject) {
             if (errorWith)
@@ -13,10 +15,21 @@ module.exports = function(resolveWith, errorWith) {
             resolveWith.splice(1, 1);
             return this;
         }
-        promise.populate = returnThis;
-        promise.exec = returnThis;
-        promise.limit = spliceThis;
-        promise.elemMatch = spliceThis;
+        promise.populate = sinon.spy(returnThis);
+        promise.where = sinon.spy(returnThis);
+        promise.and = sinon.spy(returnThis);
+        promise.where = sinon.spy(returnThis);
+        promise.limit = sinon.spy(spliceThis);
+        promise.elemMatch = sinon.spy(spliceThis);
+        promise.exec = sinon.spy(returnThis);
+        if (assert) {
+            if (!assert_on) assert_on = 'exec';
+            var fn = promise[assert_on];
+            promise[assert_on] = function() {
+                assert.apply(this);
+                return fn();
+            }
+        }
         return promise;
     }
 }
