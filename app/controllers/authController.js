d69c94d09642537553925f73bc4791da240c801a
nigh7sh4de <plotnikd@gmail.com>
2016-07-26 19:33:35 -0400

    All social auth is handled by client

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -2,101 +2,25 @@ var jwt = require('jsonwebtoken');
 var URL = require('url');
 
 var authController = function(app) {
-        this.app = app;
-        app.get('/logout', this.Logout);
-
-        app.get('/login/:strat', this.Login.bind(this));
-        // app.get('/login/return', this.RedirectToLogin.bind(this));
-        app.get('/login/:strat/return', this.LoginReturn.bind(this));
-        app.post('/auth/:strat', app.bodyParser.json(), this.LoggedIn.bind(this));
-        // app.get('/auth/:strat', app.bodyParser.json(), this.LoggedIn.bind(this));
-        // app.get('/connect/:strat', this.Connect.bind(this));
-        // app.get('/connect/:strat/return', this.ConnectReturn.bind(this));
-
-    }
-
-var GenerateCallback = function(redirect, req, res) {
-    var config = this.app.config;
-    // redirect = false;
-    // return redirect ? {
-    //     failureRedirect: '/#/login',
-    //     successRedirect: '/#/login/return'
-    // } : 
-    return function(err, user, info) {
-        if (err)
-            return res.sendBad(err);
-        if (!user)
-            return res.sendBad(['User could not be authenticated', info]);
-        var token = jwt.sign({id: user.id, profile: user.profile}, config.JWT_SECRET_KEY);
-        if (redirect) {
-            res.cookie('jwt', token);
-            return res.redirect('/#/login');
-        }
-        res.sendGood('User authenticated successfuly', token, {status: 302});
-    };
+    this.app = app;
+    app.post('/auth/:strat', app.bodyParser.json(), this.Authenticate.bind(this));
 }
 
 authController.prototype = {
-    app: null,
-    strategies: {
-        google: {
-            authProps: { scope: ['profile'] }
-        },
-        facebook: {}
-    },
-    GenerateToken: function(req, res) {
-        console.log(req);
-    },
-    Logout: function(req, res) {
-        req.logout();
-        res.redirect('/');
-    },
-    RedirectToLogin: function(req, res) {
-        console.log('derp');
-        res.send("Derp");
-        // res.redirect('/');
-    },
-    Login: function(req, res, next) {
-        var strat = req.params.strat;
-        // return res.sendBad(null,null,{status: 501});
-        // var url = URL.format({
-        //     protocol: req.protocol,
-        //     host: req.get('host'),
-        //     pathname: '/'
-        // })
-        // url = 'https://www.facebook.com/connect/login_success.html';
-        // if (strat == 'facebook')
-        //     res.redirect('https://www.facebook.com/dialog/oauth? ' +
-        //                  'client_id=' + this.app.config.FACEBOOK_CLIENT_ID + '&' +
-        //                  'display=popup&' + 
-        //                  'response_type=token&' +
-        //                  'redirect_uri=' + url);
-        this.app.passport.authenticate(strat, this.strategies[strat].authProps)(req, res, next);
-    },
-    LoginReturn: function(req, res, next) {
-        this.app.passport.authenticate(
-            req.params.strat,
-            GenerateCallback(true, req, res)
-        )(req, res, next);
-    },
-    LoggedIn: function(req, res, next) {
+    Authenticate: function(req, res, next) {
+        var config = this.app.config;
         this.app.passport.authenticate(
             req.params.strat + '-token',  
-            GenerateCallback(!req.body.noredirect, req, res)
+            function(err, user, info) {
+                if (err)
+                    return res.sendBad(err);
+                if (!user)
+                    return res.sendBad(['User could not be authenticated', info]);
+                var token = jwt.sign({id: user.id, profile: user.profile}, config.JWT_SECRET_KEY);
+                res.sendGood('User authenticated successfuly', token);
+            }
         )(req, res, next);
-    },
-    Connect: function(req, res, next) {
-        var strat = req.params.strat;
-        this.app.passport.authorize(strat, this.strategies[strat].authProps)(req, res, next);
-    },
-    ConnectReturn: function(req, res, next) {
-        this.app.passport.authorize(req.params.strat, {
-            failureRedirect: '/#/login',
-            successRedirect: '/#/profile'
-        })(req, res, next);
     }
 }
-
-
 
 module.exports = authController;
--- app/passport.js
+++ app/passport.js
@@ -1,80 +1,32 @@
 module.exports = function(db, config) {
     var passport = require('passport');
-    var FacebookStrategy = require('passport-facebook').Strategy;
-    var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
     var FacebookTokenStrategy = require('passport-facebook-token');
     var GoogleTokenStrategy = require('passport-google-token').Strategy;
     var JwtStrategy = require('passport-jwt').Strategy;
     var ExtractJwt = require('passport-jwt').ExtractJwt;
+
     passport.use(new JwtStrategy({
         jwtFromRequest: ExtractJwt.fromAuthHeader(),
         secretOrKey: config.JWT_SECRET_KEY,
         algorithms: ['HS256']
-    }, function(payload, done) {
-        GenericStrategy(null, null, null, payload, done, 'jwt')
+    }, function(profile, cb) {
+        db.checkUser('jwt', profile, cb);
     }))
+
     passport.use(new GoogleTokenStrategy({
         clientID: config.GOOGLE_CLIENT_ID,
         clientSecret: config.GOOGLE_CLIENT_SECRET
     }, function(accessToken, refreshToken, profile, cb) {
-            GenericStrategy(null, accessToken, refreshToken, profile, cb, 'google');
+        db.checkUser('google', profile, cb);
     }))
+    
     passport.use(new FacebookTokenStrategy({
         clientID: config.FACEBOOK_CLIENT_ID,
         clientSecret: config.FACEBOOK_CLIENT_SECRET
     }, function(accessToken, refreshToken, profile, cb) {
-            GenericStrategy(null, accessToken, refreshToken, profile, cb, 'facebook');
+        db.checkUser('facebook', profile, cb);
     }))
-    passport.use(new GoogleStrategy({
-            clientID: config.GOOGLE_CLIENT_ID,
-            clientSecret: config.GOOGLE_CLIENT_SECRET,
-            callbackURL: '/login/google/return',
-            passReqToCallback: true
-        },
-        function(req, accessToken, refreshToken, profile, cb) {
-            GenericStrategy(req, accessToken, refreshToken, profile, cb, 'google')
-        }
-    ));
-    passport.use(new FacebookStrategy({
-            clientID: config.FACEBOOK_CLIENT_ID,
-            clientSecret: config.FACEBOOK_CLIENT_SECRET,
-            callbackURL: '/login/facebook/return',
-            profileFields: ['id', 'displayName'],
-            passReqToCallback: true
-        },
-        function(req, accessToken, refreshToken, profile, cb) {
-            GenericStrategy(req, accessToken, refreshToken, profile, cb, 'facebook');
-        }
-    ));
-    var GenericStrategy = function(req, accessToken, refreshToken, profile, cb, strat) {
-        if (strat === 'jwt') {
-            db.checkUser('jwt', profile, cb);
-        }
-        else if (!req || !req.user)
-            db.checkUser(strat, profile, function(err, res) {
-                if (err)
-                    cb(err);
-                else {
-                    return cb(null, res);
-                }
-            });
-        else
-            db.connectUser(req.user, strat, profile, function(err, res) {
-                if (err)
-                    cb(err);
-                else {
-                    return cb(null, res);
-                }
-            });
-    }
-
-    passport.serializeUser(function(user, cb) {
-        return cb(null, user.id);
-    });
-
-    passport.deserializeUser(function(obj, cb) {
-        db.users.findById(obj, cb);
-    });
     
     return passport;
 }
