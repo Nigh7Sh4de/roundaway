4c8e63f31f7bf149e5f09e70b6bbd192b9599490
Nigh7Sh4de <plotnikd@gmail.com>
2017-03-22 15:10:53 +0100

    Test for stripe payment with cus token

--- app/tests/stripe.test.js
+++ app/tests/stripe.test.js
@@ -119,6 +119,25 @@ describe('Stripe', function() {
         })
     })
     describe('charge', function() {
+        it('should use a stripe customer if the cus token is provided', function() {
+            var stripe = new Stripe('some key');
+            var token = 'cus_sometoken';
+            var amount = 123.45;
+            var amountInCents = parseInt(amount * 100);
+            stripe.stripe = {
+                charges: {
+                    create: function(opt) {
+                        return Promise.resolve(opt);
+                    }
+                }
+            }
+            return stripe.charge(token, null, amount).then(function(opt) {
+                expect(opt.customer).to.equal(token);
+                expect(opt.amount).to.equal(amountInCents);
+                expect(opt.currency).to.equal('cad');
+            })
+        })
+
         it('should use a destination if provided', function() {
             var stripe = new Stripe('some key');
             var token = 'some token';
